56647021a25bbc419aa946ff9d076b2eabef5a98
RuiJ72 <rlagos900@gmail.com>
2022-03-13 08:41:39 -0300

    Correçoes ao nivel da compilaçao do typescript/para a pasta dist

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const api = (0, express_1.default)();
-api.listen(3000, () => console.log('Express listening'));
+api.listen(3000, () => console.log('Servidor rodando'));
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import express from 'express';
 
 const api = express()
 
-api.get('/test', () => {
-    res.jason({message: 'ok'})
-})
 
-api.listen(3000, () => console.log('Express listening'))
+
+api.listen(3000, () => console.log('Servidor rodando'))
